7295a4505ca08e8d3b7cc85b549df971911069cd
Hitesh Sharma <4hiteshsharma@gmail.com>
2024-09-03 16:39:38 +0530

    Create cars.py

---
+++ src/Main/templates/cars.py
@@ -0,0 +1,22 @@
+BRANDS=(
+    ('bmw', 'BMW'),
+    ('mercedes benz', 'Mercedes Benz'),
+    ('audi', 'Audi'),
+    ('subaru', 'Subaru'),
+    ('tesla', 'Tesla'),
+    ('jaguar', 'Jaguar'),
+    ('land rover', 'Land Rover'),
+    ('bentley', 'Bentley'),
+    ('bugatti', 'Bugatti'),
+    ('ferrari', 'Ferrari'),
+    ('lamborghini', 'Lamborghini'),
+    ('honda', 'Honda'),
+    ('toyota', 'Toyota'),
+    ('chevrolet', 'Chevrolet'),
+    ('porsche', 'Porsche')
+)
+
+TRANSMISSION =  (
+    ('automatic', 'Automatic'),
+    ('manual', 'Manual'),
+)
